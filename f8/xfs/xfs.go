8bcc7239d2ef980c2c5928b2eb46d0f018152b98
phanirithvij <phanirithvij2000@gmail.com>
2020-11-18 02:42:03 +0530

    backp

--- f8/xfs/xfs.go
+++ f8/xfs/xfs.go
@@ -1,23 +1,34 @@
 package xfs
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 // XFileSystem a simple filesystem implementation
 type XFileSystem struct {
+	FileDir
 }
 
 // Bucket is equivalient to a filesystem with a name
 type Bucket struct {
 	XFileSystem
-	Name string
+	/*
+		Bucket ID will not be unique as evey entity has a `default` bucket
+		Entity ID and bucket ID together will be unique
+	*/
+	// https://gorm.io/docs/composite_primary_key.html
+	ID       string `gorm:"primarykey"`
+	EntityID string `gorm:"primarykey"`
 }
 
-// File a file
-type File struct {
-	os.FileInfo
-}
+// copied from os.FileInfo interface{}
 
-// Dir a directory
-type Dir struct {
-	os.FileInfo
+// FileDir a file or directory
+type FileDir struct {
+	Name    string      // base name of the file
+	Size    int64       // length in bytes for regular files; system-dependent for others
+	Mode    os.FileMode // file mode bits
+	ModTime time.Time   // modification time
+	IsDir   bool        // abbreviation for Mode.IsDir
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/lib/pq"
 	"github.com/phanirithvij/fate/f8/entity"
@@ -13,10 +14,13 @@ import (
 
 // User a simple user struct
 type User struct {
-	gorm.Model
-	*entity.Base `gorm:"embedded"`
-	Name         string         `json:"name"`
-	Emails       pq.StringArray `gorm:"type:varchar(254)[]" json:"emails"`
+	entity.BaseEntity `gorm:"embedded"`
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	DeletedAt         gorm.DeletedAt `gorm:"index"`
+	Username          string         `gorm:"primarykey,not null" json:"username"`
+	Name              string         `json:"name"`
+	Emails            pq.StringArray `gorm:"type:varchar(254)[]" json:"emails"`
 }
 
 func (u User) String() string {
@@ -34,32 +38,40 @@ var (
 	db *gorm.DB
 )
 
+// postgres pgadmin javascript mime type unblock on windows
+// https://www.pgadmin.org/faq/
+// https://stackoverflow.com/questions/39228657/disable-chrome-strict-mime-type-checking#comment114712270_58133872
+
+const (
+	username = "postgres"
+	password = "522191"
+	hostname = "localhost"
+	port     = "5433"
+	// port     = "5432"
+	dbame = "gorm"
+)
+
 // Main entrypoint for hacky development tests
 func main() {
-	dsn := "postgres://postgres:522191@localhost:5432/gorm?sslmode=disable"
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		username, password, hostname, port, dbame,
+	)
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.Debug()
 
-	err = db.AutoMigrate(&User{})
+	err = AutoMigrate()
 	if err != nil {
 		log.Println("AutoMigrate failed")
 		log.Fatal(err)
 	}
-	// var u int
-	// tx := db.Raw("").Scan(&u)
-	// sdb, err := tx.DB()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// sdb.Query()
 
 	user := new(User)
-	user.Base = entity.NewBase()
-	user.Emails = pq.StringArray{"3", "3"}
+	user.BaseEntity = *entity.NewBase()
+	user.Emails = pq.StringArray{"pano@fm.dm", "dodo@gmm.ff"}
 	user.Name = "Phano"
 	err = user.Register()
 	if err != nil {
@@ -69,7 +81,16 @@ func main() {
 
 // Register a user
 func (u User) Register() error {
-	tx := db.Create(&u)
+	// db.DryRun = true
+	tx := db.Debug().Create(&u)
 	fmt.Println(u)
+	// fmt.Println(tx.Statement.)
 	return tx.Error
+}
+
+// AutoMigrate the user's schema
+func AutoMigrate() error {
+	u := &User{BaseEntity: *entity.NewBase()}
+	u.BaseEntity.AutoMigrate(db)
+	return db.AutoMigrate(u)
 }
